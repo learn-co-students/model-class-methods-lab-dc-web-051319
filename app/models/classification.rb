8c39042c5e4788e4fca72be7e351f9da557986f0
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-06-12 09:07:16 -0400

    Completed lab

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,14 @@
+# frozen_string_literal: true
+
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    all.distinct
+  end
+
+  def self.longest
+    Boat.order(:length).last.classifications
+  end
 end
